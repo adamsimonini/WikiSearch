e9f6e9a5c756cb1141b71a3827ed4867952850d2
Adam Simonini <adam.simonini@gmail.com>
2017-04-02 16:05:52 -0400

    Randomzier
    Added randomizer. Adjusted placement of API function

--- WikiSearchJS.js
+++ WikiSearchJS.js
@@ -3,55 +3,47 @@
 $( document ).ready(function(){
 
 var hasReturned = false;
+var wikiURL = "";
 
-  // Affix user input to variable and set URL
-  $('#searchInput').keydown(function(event){
-      if (event.keyCode == 13){
-        hasReturned = true;
-        userInput = $('#searchInput').val();
-        wikiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + userInput + "&format=json";
-        $('#searchInput').val("");
-        $('#searchInput').replaceWith('<input id="searchInput" class="form-style-4" type="text" style="text-align:center" placeholder="search again?" name="field1" required="true" />');
-
-        //Reset the value of input field to blank
-
-
-        //Make AJAX Request to Media Wiki
-        $.ajax({
-          type:"GET",
-          url: wikiURL,
-          async: false,
-          dataType: "jsonp",
-          success: function(data){
-            console.log(data);
-
-           }
-         });
-      }else{
-          return;
-      }
-    });
-  });
-/*
-   // Use URL to perform AJAX GET Request
-      if(hasReturned){
-
+//Make AJAX Request to Media Wiki
+  function APICall(){
           $.ajax({
             type:"GET",
             url: wikiURL,
             async: false,
-            dataType: "json",
+            dataType: "jsonp",
             success: function(data){
-              alert(data);
-
-             }
+            $("#searchResults").append("<a href='" + data[3][0] + "'>" + data[0] + "</a>");
+            $("#searchResults").append("<p>" + data[2][0] + "</p>");
+            $("#searchResults").append("<p>" + data[3][0] + "</p>");
+            }
            });
-       }else{
+  }
+
+//Affix user input to variable and set URL
+    $('#searchInput').keydown(function(event){
+      if (event.keyCode == 13){
+        hasReturned = true;
+        userInput = $('#searchInput').val();
+        wikiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + userInput + "&format=json";
+
+        //Reset the value of input field to "search again?"
+        $('#searchInput').val("");
+        $('#searchInput').replaceWith('<input id="searchInput" class="form-style-4" type="text" style="text-align:center" placeholder="search again?" name="field1" required="true" />');
+
+        APICall();
+        }else{
           return;
-       }
-     });
-});
-*/
+        }
+    });
+    
+//Randomizer Button JS
+    $('#randomizer').click(function(){
+      wikiURL = "https://en.wikipedia.org/w/api.php?action=query&list=random&rnlimit=5";
+      APICall();
+    });
+  });
+
 //Auto Expand Text Area For Style
 
   function adjust_textarea(h) {
